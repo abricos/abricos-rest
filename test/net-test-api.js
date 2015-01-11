57cec1e3ad9cb1b4e087e538f6c1a4bd875f2ea9
Alexander Kuzmin <roosit@abricos.org>
2015-01-11 23:17:58 +0300

    draft: user auth

--- test/net-test-api.js
+++ test/net-test-api.js
@@ -4,25 +4,46 @@ var Abricos = require('../index');
 var should = require('should');
 
 describe('Abricos.API Module Network Functions:', function () {
-  describe('API.post()', function () {
+
+  describe('User API', function () {
     it('should send a current user info', function (done) {
       this.timeout(40000);
 
       var api = new Abricos.API();
-
-      api.should.be.an.instanceOf(Object);
-
       var userModule = api.getModule('user');
 
       userModule.userCurrent(function (err, userCurrnet) {
         should.not.exist(err);
 
+        userCurrnet.should.have.property('id');
+        userCurrnet.should.have.property('username');
+        userCurrnet.should.have.property('permission');
+
         should.exist(userCurrnet);
 
         done();
       });
-
     });
   });
 
+  describe('User API', function () {
+    it('should admin user authorization', function (done) {
+      this.timeout(40000);
+
+      var api = new Abricos.API();
+      var userModule = api.getModule('user');
+
+      userModule.auth(function (err, userCurrnet) {
+        should.not.exist(err);
+
+        userCurrnet.should.have.property('id');
+        userCurrnet.should.have.property('username');
+        userCurrnet.should.have.property('permission');
+
+        should.exist(userCurrnet);
+
+        done();
+      });
+    });
+  });
 });
