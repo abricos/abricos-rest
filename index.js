72db5024951bdac9469ea3c5cf4fbd210b4691d7
Alexander Kuzmin <roosit@abricos.org>
2017-03-11 20:39:27 +0300

    source refactoring

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 var API = require('./lib/API');
 var Module = require('./lib/Module');
 var helper = require('./lib/helper');
+var cheerio = require('cheerio');
 
 module.exports.API = API;
 module.exports.Module = Module;
 module.exports.helper = helper;
+module.exports.cheerio = cheerio;
